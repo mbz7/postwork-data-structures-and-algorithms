8f786d076597b15b79f9965cc9b6aa30fc162a3b
Myles <bracken.myles@gmail.com>
2022-05-12 21:48:50 -0700

    completed answer

--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
@@ -1,6 +1,18 @@
 function fibonacci(num) {
-  // type your code here
+  if (num < 2) {
+    return num;
+  }
+
+  let lastTwo = [0, 1];
+
+  for (i = 0; i < num - 1; i++) {
+    const sum = lastTwo[0] + lastTwo[1];
+    lastTwo = [lastTwo[1], sum];
+  }
+  return lastTwo[1];
 }
+//  0 1 2 3 4 5 6  7  8  9  10 - index
+// [0,1,1,2,3,5,8,13,21,34] - array numbers
 
 if (require.main === module) {
   // add your own tests in here
